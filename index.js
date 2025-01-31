5efabb4eb4ef6ea7438678c5bf6beb683fdc02ed
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-01-31 11:58:13 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-const port = 3010;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('static'));
+// Connect to MongoDB
+const mongoURI = process.env.MONGO_URI;
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Error connecting to database: ${error.message}`);
+  });
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("Customer Management System is running!");
 });
